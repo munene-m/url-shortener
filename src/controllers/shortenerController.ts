55ca7d9f4b739ea629b806046ddb19860e0f03b1
munene-m <munenenjue18@gmail.com>
2023-12-18 19:47:09 +0300

    chore: controller to shorten long urls and redirect users to original urls

---
+++ src/controllers/shortenerController.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import validUrl from "valid-url";
+import { PrismaClient } from "@prisma/client";
+import { nanoid } from "nanoid";
+const prisma = new PrismaClient();
+
+interface ShortenUrlRequest {
+  url: string;
+}
+const CLIENT_URL = process.env.CLIENT_URL;
+
+export const shortenUrl = async (req: Request, res: Response) => {
+  const { url } = req.body as ShortenUrlRequest;
+
+  if (!validUrl.isUri(url)) {
+    return res.status(400).json({ error: "Invalid URL format" });
+  }
+
+  const shortIdentifier = nanoid(7);
+  const shortUrl = `${CLIENT_URL}/url/${shortIdentifier}`;
+
+  const createdUrl = await prisma.url.create({
+    data: {
+      originalUrl: url,
+      shortUrl,
+    },
+  });
+  res.status(200).json({ shortUrl: createdUrl.shortUrl });
+};
+
+export const redirectToOriginal = async (req: Request, res: Response) => {
+  const { shortIdentifier } = req.params;
+  try {
+    const url = await prisma.url.findFirst({
+      where: {
+        shortUrl: `${CLIENT_URL}/url/${shortIdentifier}`,
+      },
+    });
+    if (!url) {
+      return res.status(404).json({ message: "Short url not found" });
+    }
+
+    res.redirect(url.originalUrl);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
